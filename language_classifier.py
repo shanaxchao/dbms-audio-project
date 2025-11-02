a0ea9d9d09107e6c16f8a84fab4a8ab290656ba7
holydeepwater <ahnseo04@gmail.com>
2025-11-02 22:51:54 +0900

    change train / test data set

--- language_classifier.py
+++ language_classifier.py
@@ -13,49 +13,63 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-# gather all audio files
-audio_files = glob("./data/*/*.wav")
+train_speakers_en = ["bea", "josh", "sam"]
+test_speakers_en = ["jenie"]
 
-X = []  # features
-y = []  # labels
+english_dir = "./data/english"
+korean_dir = "./data/korean"
 
-for file in audio_files:
-    path = Path(file)
-    label = path.parent.name  # get folder name, e.g. 'korean'
-    y.append(label)
-    
-    # load audio
-    y_audio, sr = librosa.load(file, sr=16000, mono=True)
-    
-    # extract features, e.g., MFCCs and average over time
-    mfccs = librosa.feature.mfcc(y=y_audio, sr=sr, n_mfcc=13)
-    mfccs_mean = np.mean(mfccs, axis=1)  # shape (13,)
+def extract_features(file):
+    y_audio, sr = librosa.load(file, sr = 16000, mono = True)
+    mfccs = librosa.feature.mfcc(y = y_audio, sr = sr, n_mfcc = 13)
+    mfccs_mean = np.mean(mfccs, axis = 1)
     mfccs_std = np.std(mfccs, axis = 1)
+    return np.concatenate((mfccs_mean, mfccs_std)).astype(np.float32)
 
-    features = np.concatenate((mfccs_mean, mfccs_std))
+#english
+english_files = glob(f"{english_dir}/*.wav")
+eng_train, eng_test = [], []
 
-    X.append(features)
+for f in english_files:
+    name = Path(f).stem.lower()
+    if any(name.startswith(spk.lower()) for spk in train_speakers_en):
+        eng_train.append(f)
+    else:
+        eng_test.append(f)
 
-X = np.array(X)
-y = np.array(y)
 
-le = LabelEncoder()
-y = le.fit_transform(y)
+#korean
+korean_files = glob(f"{korean_dir}/*.wav")
+kor_train = [f for f in korean_files if Path(f).stem.lower().startswith("a_")]
+kor_test = [f for f in korean_files if Path(f).stem.lower().startswith("b_")]
 
-# split into training and testing sets
-for seed in [1, 2, 3, 4, 5]:
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size = 0.2, random_state = seed, stratify = y
-    )
+print(len(eng_test), len(kor_test))
 
-# train random forest
-#model = RandomForestClassifier(n_estimators=100, random_state=40)
+#data split
+X_train, y_train, X_test, y_test = [], [], [], []
+for f in eng_train:
+    X_train.append(extract_features(f))
+    y_train.append(0) # 0 == eng
+for f in eng_test:
+    X_test.append(extract_features(f))
+    y_test.append(0)
+
+for f in kor_train:
+    X_train.append(extract_features(f))
+    y_train.append(1) # 1 == kor
+for f in kor_test:
+    X_test.append(extract_features(f))
+    y_test.append(1)
+
+# train XGBoost
 model = xgb.XGBClassifier(
     n_estimators = 300, 
-    learning_rate = 0.3, 
+    learning_rate = 0.1, 
     max_depth = 6, 
-    subsample = 1,
-    eval_metric = 'logloss'
+    subsample = 0.9,
+    colsample_bytree = 0.9,
+    eval_metric = 'logloss',
+    random_state = 42
 )
 
 model.fit(X_train, y_train)
@@ -67,11 +81,14 @@ y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
 # detailed report
-print(classification_report(y_test, y_pred, target_names = le.classes_))
+print(classification_report(y_test, y_pred, 
+                            labels = [0, 1],
+                            target_names = ["english", "korean"], 
+                            zero_division = 0))
 
 # confusion matrix
 print(confusion_matrix(y_test, y_pred))
 
 # save model
 Path("models").mkdir(exist_ok=True)
-joblib.dump({"model":model, "label_encoder":le}, "models/language_classifier.pkl")
+joblib.dump(model, "models/language_classification.pkl")
